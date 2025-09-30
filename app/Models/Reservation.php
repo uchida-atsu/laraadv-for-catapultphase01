a854cb8f91eab32be54c52eb5df4b3729b00f38b
uchida-atsu <uchida.atsuro@gmail.com>
2025-09-30 14:02:22 +0900

    Reservationモデルの作成、予約一覧・予約画面へのリンク（viewはまだ手着けてない）

---
+++ app/Models/Reservation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReservationFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'reserved_at',
+        'purpose',
+    ];
+    /**
+     * relation to User
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_09_30_040715_create_reservations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->dateTime('reserved_at'); // 予約日時（例: 2025-10-01 10:00）
+            $table->string('purpose');       // 利用目的
+            $table->timestamps();
+
+            $table->unique(['user_id', 'reserved_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
